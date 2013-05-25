6b53df6a55f2c6309eb5a7793e9be26b6f7b4de6
Chino <joseignaciosg9@gmail.com>
2013-05-25 16:31:18 -0300

    first commit

---
+++ src/main/java/olap/olap/project/App.java
@@ -0,0 +1,13 @@
+package olap.olap.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
