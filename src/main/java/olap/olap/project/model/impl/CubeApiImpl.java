ee0a7154aad3f885a616375d09a0758f2a6e0798
Federico Ezequiel Di Nucci <dinu1389@gmail.com>
2013-06-20 14:40:37 -0300

    Creacion de Fact Table

--- src/main/java/olap/olap/project/model/impl/CubeApiImpl.java
+++ src/main/java/olap/olap/project/model/impl/CubeApiImpl.java
@@ -3,18 +3,24 @@ package olap.olap.project.model.impl;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import org.dom4j.DocumentException;
+import java.util.Map;
+import java.util.Set;
 
 import olap.olap.project.model.Cube;
 import olap.olap.project.model.Dimension;
+import olap.olap.project.model.Measure;
 import olap.olap.project.model.MultiDim;
+import olap.olap.project.model.Property;
 import olap.olap.project.model.api.CubeApi;
 import olap.olap.project.model.db.ConnectionManager;
 import olap.olap.project.model.db.ConnectionManagerPostgreWithCredentials;
 import olap.olap.project.xml.XmlConverter;
+
+import org.dom4j.DocumentException;
 
 public class CubeApiImpl implements CubeApi {
 
@@ -28,7 +34,7 @@ public class CubeApiImpl implements CubeApi {
 	public boolean setDBCredentials(String dbUrl, String name, String password) {
 		connectionManager = ConnectionManagerPostgreWithCredentials
 				.setConnectionManagerWithCredentials(dbUrl, name, password);
-		
+
 		return false;
 	}
 
@@ -43,6 +49,13 @@ public class CubeApiImpl implements CubeApi {
 
 		XmlConverter xml = new XmlConverter();
 		xml.generateXml(multiDim, outFileName);
+
+		try {
+			createFactTable();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public List<Dimension> getCubeDimensions() {
@@ -64,4 +77,74 @@ public class CubeApiImpl implements CubeApi {
 		// TODO Auto-generated method stub
 	}
 
+	private void createFactTable() throws Exception {
+
+		final Connection conn = connectionManager
+				.getConnectionWithCredentials();
+
+		Map<Integer, String> parameters = new HashMap<Integer, String>();
+		int key = 1;
+		// -----------------------------------------
+		// PREPARO EL STATEMENT
+		String query = "CREATE TABLE ? { \n";
+		parameters.put(key++, cube.getName());
+		// -----------------------------------------
+		// AGREGO LAS MEASURES
+
+		for (Measure m : cube.getMeasures()) {
+			parameters.put(key++, m.getName());
+			parameters.put(key, m.getType());
+			query += "? ? ,\n";
+		}
+		// -----------------------------------------
+		// AGREGO LAS DIMENSIONES Y LAS FOREIGN KEYS
+		ArrayList<Dimension> dimensions = (ArrayList<Dimension>) cube
+				.getDimensions().values();
+		for (Dimension d : dimensions) {
+			Set<Property> properties = d.getLevel().getProperties();
+			for (Property p : properties) {
+				if (p.isPK()) {
+					parameters.put(key++, p.getName());
+					parameters.put(key++, p.getType());
+					parameters.put(key++, d.getName());
+					parameters.put(key++, p.getName());
+					query += "? ? REFERENCES ?(?),\n";
+				}
+			}
+		}
+		// -----------------------------------------
+		// AGREGO LAS PRIMARY KEYS
+		query += "PRIMARY KEY (";
+		boolean first = true;
+		for (Dimension d : dimensions) {
+			Set<Property> properties = d.getLevel().getProperties();
+			for (Property p : properties) {
+				if (p.isPK()) {
+					parameters.put(key++, p.getName());
+					if (!first) {
+						query += ",?";
+					} else {
+						query += "?";
+					}
+
+				}
+			}
+		}
+		query += " );\n";
+		// -----------------------------------------
+		// PRINTEO LA QUERY
+		System.out.println(query);
+		for (int i = 1; i < key; i++) {
+			System.out.println(i + ": " + parameters.get(i));
+		}
+		PreparedStatement statement = conn.prepareStatement(query,
+				PreparedStatement.RETURN_GENERATED_KEYS);
+
+		for (int i = 1; i < key; i++) {
+			statement.setString(i, parameters.get(i));
+		}
+		statement.execute();
+		connectionManager.closeConnection(conn);
+
+	}
 }
